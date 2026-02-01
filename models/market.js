374f02dee3e70aae651aedb8213efe1c7e4fd2c2
frostbite4575 <frostbite4575@gmail.com>
2026-02-01 11:52:38 -0600

    added market models and got a base line set up for further tinkering

---
+++ models/market.js
@@ -0,0 +1,47 @@
+const { db } = require('../config/firebase');
+const { admin } = require('../config/firebase');
+
+//spy oil gold bitcoin nvidia
+class Market {
+    constructor(data) {
+        this.id = data.id;
+        this.spy = data.spy;
+        this.gold = data.gold;
+        this.bitcoin = data.bitcoin;
+        this.oil = data.oil;
+        this.nvidia = data.nvidia;
+    }
+
+
+// Create new market
+static async create(marketData) {
+    const market = new Market(marketData);
+    const docRef = await db.collection('market').add({
+      ...marketData,
+      createdAt: admin.firestore.FieldValue.serverTimestamp()
+    });
+    
+    return {
+      id: docRef.id,
+      ...market
+    };
+  }
+
+//find them
+    static async findAll() {
+    const snapshot = await db.collection('market')
+      .orderBy('createdAt', 'desc')
+      .get();
+    
+    const market = [];
+    snapshot.forEach(doc => {
+      market.push({
+        id: doc.id,
+        ...doc.data()
+      });
+    });
+    
+    return worlds;
+  }
+}
+module.exports = db.collection('market');
